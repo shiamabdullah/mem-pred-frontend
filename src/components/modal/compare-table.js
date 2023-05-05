1792f0920261d15fda5efd02ba68405df8a5f0bd
shiam <amamun.shiam@tetonelectronics.com>
2023-05-05 15:25:30 +0600

    updated

--- src/components/modal/compare-table.js
+++ src/components/modal/compare-table.js
@@ -1,19 +1,46 @@
-import { Tooltip } from '@mui/material';
-import Paper from '@mui/material/Paper';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import * as React from 'react';
+import { Tooltip } from "@mui/material";
+import Paper from "@mui/material/Paper";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import * as React from "react";
 
-const rows = ['Comp', 'Type', 'Words', 'Bits', 'Area_umA2', 'tacc_ns_ffg', 'tcycle_ns_ffg', 'tsetup_ns_ffg', 'thold_ns_ffg', 'read_power_pj_ffg', 'write_power_pj_ffg', 'tacc_ns_ssg', 'tcycle_ns_ssg', 'tsetup_ns_ssg', 'thold_ns_ssg', 'read_power_pj_ssg', 'write_power_pj_ssg', 'tacc_ns_tt', 'tcycle_ns_tt', 'tsetup_ns_tt', 'thold_ns_tt', 'read_power_pj_tt', 'write_power_pj_tt', 'leakage_power_mw_ffg', 'leakage_power_mw_ssg', 'leakage_power_mw_tt']
+const rows = [
+  "Comp",
+  "Type",
+  "Words",
+  "Bits",
+  "Area_umA2",
+  "tacc_ns_ffg",
+  "tcycle_ns_ffg",
+  "tsetup_ns_ffg",
+  "thold_ns_ffg",
+  "read_power_pj_ffg",
+  "write_power_pj_ffg",
+  "tacc_ns_ssg",
+  "tcycle_ns_ssg",
+  "tsetup_ns_ssg",
+  "thold_ns_ssg",
+  "read_power_pj_ssg",
+  "write_power_pj_ssg",
+  "tacc_ns_tt",
+  "tcycle_ns_tt",
+  "tsetup_ns_tt",
+  "thold_ns_tt",
+  "read_power_pj_tt",
+  "write_power_pj_tt",
+  "leakage_power_mw_ffg",
+  "leakage_power_mw_ssg",
+  "leakage_power_mw_tt",
+];
 
 function getDifference(num1, num2) {
   var difference = num1 - num2;
   var average = num1 + num2 / 2;
-  var percentageDifference = difference / average * 100;
+  var percentageDifference = (difference / average) * 100;
 
   if (Number.isInteger(percentageDifference)) {
     return percentageDifference;
@@ -31,7 +58,22 @@ function formatNumber(number) {
     return number.toFixed(3);
   }
 }
+const darkColors = [
+  "#003f5c",
+  "#2f4b7c",
+  "#665191",
+  "#a05195",
+  "#d45087",
+  "#f95d6a",
+  "#ff7c43",
+  "#ffa600",
+  "#005792",
+  "#0086a8",
+];
 
+function getRandomColor() {
+  return darkColors[Math.floor(Math.random() * darkColors.length)];
+}
 
 export default function CompareTable({ selectedData }) {
   const selectedDataKeys = Object.keys(selectedData);
@@ -39,65 +81,77 @@ export default function CompareTable({ selectedData }) {
   // console.log(selectedDataKeys)
 
   return (
-    <Paper sx={{ width: '100%', overflow: 'hidden' }}>
+    <Paper sx={{ width: "100%", overflow: "hidden" }}>
       <TableContainer sx={{ maxHeight: 440 }}>
-        <Table stickyHeader aria-label="sticky table">
+        <Table
+          stickyHeader
+          aria-label="sticky table"
+          options={{
+            exportButton: true,
+          }}
+        >
           <TableHead>
             <TableRow>
-              <TableCell
-                style={{ minWidth: 180 }}
-              >
-                Predictions
-              </TableCell>
-              {selectedDataKeys.length > 0 && selectedDataKeys.map((column, index) => (
-                <TableCell
-                  key={index}
-                  style={{ minWidth: 180 }}
-                >
-                  {column?.length > 12 ? <Tooltip title={column}><span>{column.slice(0, 12) + "..."}</span></Tooltip> : column}
-                </TableCell>
-              ))}
-              {selectedDataKeys.length === 2 &&
-                <TableCell
-                  style={{ minWidth: 180 }}
-                >
-                  % Difference
-                </TableCell>
-              }
+              <TableCell style={{ minWidth: 180 }}>Predictions</TableCell>
+              {selectedDataKeys.length > 0 &&
+                selectedDataKeys.map((column, index) => (
+                  <TableCell
+                    key={index}
+                    style={{
+                      minWidth: 180,
+                      color:
+                        index === 0
+                          ? "#7149C6"
+                          : index === 1
+                          ? "#FF6D60"
+                          : getRandomColor(),
+                    }}
+                  >
+                    {column?.length > 12 ? (
+                      <Tooltip title={column}>
+                        <span>{column.slice(0, 12) + "..."}</span>
+                      </Tooltip>
+                    ) : (
+                      column
+                    )}
+                  </TableCell>
+                ))}
+              {selectedDataKeys.length === 2 && (
+                <TableCell style={{ minWidth: 180 }}>% Difference</TableCell>
+              )}
             </TableRow>
           </TableHead>
           <TableBody>
             {rows.map((row, i) => {
               return (
                 <TableRow hover tabIndex={-1} key={i}>
-                  <TableCell>
-                    {row}
-                  </TableCell>
-                  {selectedDataKeys.length > 0 && selectedDataKeys.map((column, index) => (
-                    <TableCell
-                      key={index}
-                      style={{ minWidth: 180 }}
-                    >
-                      {
-                        typeof JSON.parse(selectedData[column])[row] !== "number" || row === "Words" || row === "Bits" ?
-                          JSON.parse(selectedData[column])[row] : formatNumber(JSON.parse(selectedData[column])[row])
-                      }
-                    </TableCell>
-                  ))}
-                  {
-                    selectedDataKeys.length === 2 &&
-
+                  <TableCell>{row}</TableCell>
+                  {selectedDataKeys.length > 0 &&
+                    selectedDataKeys.map((column, index) => (
+                      <TableCell key={index} style={{ minWidth: 180 }}>
+                        {typeof JSON.parse(selectedData[column])[row] !==
+                          "number" ||
+                        row === "Words" ||
+                        row === "Bits"
+                          ? JSON.parse(selectedData[column])[row]
+                          : formatNumber(JSON.parse(selectedData[column])[row])}
+                      </TableCell>
+                    ))}
+                  {selectedDataKeys.length === 2 && (
                     <TableCell>
-                      {
-                        typeof JSON.parse(selectedData[selectedDataKeys[0]])[row] === "number" && typeof JSON.parse(selectedData[selectedDataKeys[1]])[row] === "number" ?
-                          getDifference(
+                      {typeof JSON.parse(selectedData[selectedDataKeys[0]])[
+                        row
+                      ] === "number" &&
+                      typeof JSON.parse(selectedData[selectedDataKeys[1]])[
+                        row
+                      ] === "number"
+                        ? getDifference(
                             JSON.parse(selectedData[selectedDataKeys[0]])[row],
                             JSON.parse(selectedData[selectedDataKeys[1]])[row]
                           )
-                          : "-"
-                      }
+                        : "-"}
                     </TableCell>
-                  }
+                  )}
                 </TableRow>
               );
             })}
--- src/components/modal/predictionModal.js
+++ src/components/modal/predictionModal.js
@@ -1,30 +1,28 @@
 // @flow strict
 
-import { Typography } from '@mui/material';
-import * as React from 'react';
-import CompareTable from './compare-table';
+import { Typography } from "@mui/material";
+import * as React from "react";
+import CompareTable from "./compare-table";
 
 function PredictionModal({ selectedData }) {
   const selectedDataKeys = Object.keys(selectedData);
 
   return (
     <div>
-      <Typography sx={{mb: 3}} align='center' variant="h5" component="h5">
-        {selectedDataKeys.length === 2 ?
-         selectedDataKeys[0] + ' vs ' + selectedDataKeys[1]
-          : "Compare Prediction"}
+      <Typography sx={{ mb: 3 }} align="center" variant="h5" component="h5">
+        {selectedDataKeys.length === 2 ? (
+          <>
+            <span style={{ color: "#7149C6" }}>{selectedDataKeys[0]}</span>
+            {" vs "}
+            <span style={{ color: "#FF6D60" }}>{selectedDataKeys[1]}</span>
+          </>
+        ) : (
+          <span>Compare Prediction</span>
+        )}
       </Typography>
       <CompareTable selectedData={selectedData} />
     </div>
   );
-};
+}
 
 export default PredictionModal;
-
-
-
-
-
-
-
-
